bb2bbbe7a244f2ce7843c0300134f46e0c03652e
ali-hajeb <ali.hajeb@hotmail.com>
2024-07-13 17:46:54 +0330

    Added Routes

--- modules/index.js
+++ modules/index.js
@@ -1,9 +1,13 @@
 const userRoutes = require('./user/routes');
 const adminRoutes = require('./admin/routes');
+const doctorRoutes = require('./doctor/routes');
+const nobatRoutes = require('./nobat/routes');
 const medRoutes = require('./med/routes');
 
 module.exports = (app) => {
   app.use('/api/v1/admin', adminRoutes);
   app.use('/api/v1/user', userRoutes);
+  app.use('/api/v1/doctor', doctorRoutes);
+  app.use('/api/v1/nobat', nobatRoutes);
   app.use('/api/v1/med', medRoutes);
 };
