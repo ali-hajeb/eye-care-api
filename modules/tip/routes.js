68c8350e28e9e69ce848ad76807eb14a93207ae5
ali-hajeb <ali.hajeb@hotmail.com>
2024-10-09 02:14:43 +0330

    Fixed

--- modules/tip/routes.js
+++ modules/tip/routes.js
@@ -7,7 +7,7 @@ const router = new Router();
 router.post('/', tipControllers.addTip);
 router.delete('/:id', jwtDoctorAuth, tipControllers.deleteTip)
 router.patch('/:id', jwtDoctorAuth, tipControllers.updateTip)
-router.get('/doc', jwtDoctorAuth, tipControllers.getTips)
+router.get('/doc', jwtDoctorAuth, tipControllers.getTipsDoc)
 router.get('/:id', tipControllers.getTipById)
 router.get('/', tipControllers.getTips)
 
